99858c77711e0652cfe6b003b29f883873e57e86
Slavko Bosancic <slavko.bosancic@gmail.com>
2018-01-27 00:16:41 +0100

    adding --release flag to build.bat script

---
+++ src/build.bat
@@ -0,0 +1,6 @@
+@echo off
+
+if "%~1"=="--release" (set var1=O3) else set var1=g4
+
+xcopy /s /y .\html ..\build
+emcc WebAssemblyTesting.cpp MovableGameObject.cpp GameObject.cpp GameEngine.cpp -std=c++11 -%var1% -s WASM=1 -s EXPORTED_FUNCTIONS="['_main', '_MoveBall']" -s EXTRA_EXPORTED_RUNTIME_METHODS="['ccall', 'cwrap']" -o ./../build/pong.js
--- src/build_debug.bat
+++
@@ -1,2 +0,0 @@
-xcopy /s /y .\html ..\build
-emcc WebAssemblyTesting.cpp MovableGameObject.cpp GameObject.cpp GameEngine.cpp -std=c++11 -g4 -s WASM=1 -s EXPORTED_FUNCTIONS="['_main', '_MoveBall']" -s EXTRA_EXPORTED_RUNTIME_METHODS="['ccall', 'cwrap']" -o ./../build/pong.js
